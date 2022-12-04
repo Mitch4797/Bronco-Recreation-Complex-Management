5fd8c31b040eb2e238f600aa9a3fc61d2f13cd88
AntoineSi <alsi@cpp.edu>
2022-12-04 13:35:03 -0800

    Beginning of JavaFX linkage
    Making FXML files and linking screens together

--- Brcm/src/main/java/com/cpp/Brcm/controllers/LandingPageController.java
+++ Brcm/src/main/java/com/cpp/Brcm/controllers/LandingPageController.java
@@ -3,15 +3,29 @@ import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.io.IOException;
+
 
 
 public class LandingPageController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
 
+
+	public void buttonClicked(ActionEvent event) throws IOException {
+		 root = FXMLLoader.load(getClass().getResource("/FXML/LoginPageController.fxml"));
+		 stage =(Stage) ((Node) event.getSource()).getScene().getWindow();
+		 scene = new Scene(root);
+		 stage.setScene(scene);
+		 stage.show();
+	}
 }
--- Brcm/src/main/java/com/cpp/Brcm/controllers/LoginPageController.java
+++ Brcm/src/main/java/com/cpp/Brcm/controllers/LoginPageController.java
@@ -1,5 +1,30 @@
 package com.cpp.Brcm.controllers;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginPageController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+
+
+	public void NewUserbuttonClicked(ActionEvent event) throws IOException {
+		 root = FXMLLoader.load(getClass().getResource("/FXML/GeneralNewUserController.fxml"));
+		 stage =(Stage) ((Node) event.getSource()).getScene().getWindow();
+		 scene = new Scene(root);
+		 stage.setScene(scene);
+		 stage.show();
+	}
 
 }
