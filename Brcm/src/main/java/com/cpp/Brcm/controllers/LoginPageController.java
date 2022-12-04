83b204e54b8a763d5190df576f6d97f832ed59eb
AntoineSi <alsi@cpp.edu>
2022-12-04 15:20:07 -0800

    FXML files
    created all FXML and controller classes

--- Brcm/src/main/java/com/cpp/Brcm/controllers/LoginPageController.java
+++ Brcm/src/main/java/com/cpp/Brcm/controllers/LoginPageController.java
@@ -2,6 +2,7 @@ package com.cpp.Brcm.controllers;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -9,15 +10,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.function.UnaryOperator;
 
 public class LoginPageController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
-
+	
 
 	public void NewUserbuttonClicked(ActionEvent event) throws IOException {
 		 root = FXMLLoader.load(getClass().getResource("/FXML/GeneralNewUserController.fxml"));
